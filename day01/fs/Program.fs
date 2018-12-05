15d2be66111437949ccd4f74559c83f824c9e8d3
Falko <falko.dulat@gmail.com>
2018-12-04 17:14:43 -0800

    First day solved using F#

---
+++ day01/fs/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let lines = File.ReadLines("../input")
+    let sum = Seq.sumBy (fun x-> (int x)) (lines |> Seq.filter(fun x-> x <> String.Empty))
+    printfn "Frequency shift: %d" sum
+    0 // return an integer exit code
+    
