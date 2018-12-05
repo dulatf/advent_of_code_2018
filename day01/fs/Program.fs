51420c2b6f03606c520c546347c42ce193c7f503
Falko <falko.dulat@gmail.com>
2018-12-04 17:16:59 -0800

    Cleanup

--- day01/fs/Program.fs
+++ day01/fs/Program.fs
@@ -1,12 +1,10 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open System.IO
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Advent of Code -- Day 01"
+    printfn "Computing frequency shift... or something..."
     let lines = File.ReadLines("../input")
     let sum = Seq.sumBy (fun x-> (int x)) (lines |> Seq.filter(fun x-> x <> String.Empty))
     printfn "Frequency shift: %d" sum
-    0 // return an integer exit code
-    
+    0
