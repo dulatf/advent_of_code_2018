51db58d92fe21b3d786217e12f45730c1db17075
Falko <falko.dulat@gmail.com>
2018-12-07 14:01:33 -0800

    Part 1 of Day 2 solved with more stream processing

---
+++ day02/fs/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.IO
+
+// Count letter frequencies and check if any letter appears exacly 2 or 3 times
+let processid idstring =
+    let seq = idstring |> Seq.countBy id |> Seq.map (fun x-> (snd x))
+    (Seq.contains 2 seq, Seq.contains 3 seq)
+
+let computeChecksum file = 
+    let lines = File.ReadLines(file)
+    let seq = Seq.map processid lines
+    let doubles = seq |> Seq.countBy (fst) |> Seq.filter (fst)
+    let triples = seq |> Seq.countBy (snd) |> Seq.filter (fst)
+
+    let doubleCount = doubles |> Seq.map (snd) |> Seq.exactlyOne
+    let tripleCount = triples |> Seq.map (snd) |> Seq.exactlyOne
+    (doubleCount, tripleCount, doubleCount*tripleCount)
+[<EntryPoint>]
+let main argv =
+    printfn "Computing package ID checksum..."
+    let (doubles, triples, checksum) = computeChecksum("../input")
+    printfn "Doubles: %d\nTriples: %d\nChecksum: %d" doubles triples checksum
+    0 // return an integer exit code
