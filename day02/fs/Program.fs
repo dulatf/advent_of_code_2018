f1c30350d39eb0753dd110683bd49cf6052a0259
Falko <falko.dulat@gmail.com>
2018-12-09 14:28:47 -0800

    Solved part 2 of day02

--- day02/fs/Program.fs
+++ day02/fs/Program.fs
@@ -15,9 +15,46 @@ let computeChecksum file =
     let doubleCount = doubles |> Seq.map (snd) |> Seq.exactlyOne
     let tripleCount = triples |> Seq.map (snd) |> Seq.exactlyOne
     (doubleCount, tripleCount, doubleCount*tripleCount)
+
+
+let compareIds (id1:string) (id2:string) =
+    Seq.map2 (( = )) id1 id2
+    |> Seq.countBy id
+    |> Seq.filter (fun x -> not (fst x))
+    |> Seq.map snd
+    |> Seq.exactlyOne
+
+let cartesianProduct xs ys =
+    //xs |> Seq.collect (fun x-> ys |> Seq.map (fun y-> x, y))
+    [for x in xs do
+        for y in ys do
+            yield (x,y)]
+
+
+let removeNonEqual (s1:string) (s2:string) =
+    Seq.map2 (fun c1 c2 -> ((c1, c2), c1=c2)) s1 s2
+    |> Seq.filter snd
+    |> Seq.map (fst >> fst)
+    |> String.Concat
+
+let findSimilarIds file = 
+    let lines = File.ReadLines(file) |> List.ofSeq
+    cartesianProduct lines lines
+    |> Seq.filter (fun (x,y) -> not (x = y))
+    |> Seq.map (fun (x,y) -> ((x, y), compareIds x y))
+    |> Seq.filter (fun ((x,y),c) -> c=1)
+    |> Seq.map (fun ((x,y),_) -> removeNonEqual x y)
+    
+
 [<EntryPoint>]
 let main argv =
     printfn "Computing package ID checksum..."
     let (doubles, triples, checksum) = computeChecksum("../input")
     printfn "Doubles: %d\nTriples: %d\nChecksum: %d" doubles triples checksum
+    (compareIds "abcde" "axcye")  
+        |> printfn "%O"
+    
+    printfn "Finding similar IDs..."
+    findSimilarIds "../input"
+    |> Seq.iter (fun x -> printfn "%s" x)
     0 // return an integer exit code
